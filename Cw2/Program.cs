4c1e7949db25ffe1b7ef6fe1b0655932b0bc8327
s19027 <s19027@pjwstk.edu.pl>
2020-03-10 16:20:39 +0100

    commit -m init

--- Cw2/Program.cs
+++ Cw2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Cw2
 {
@@ -6,7 +7,52 @@ namespace Cw2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //git status -> git add .->git status -> git commit -m <nazwa> ->git push
+
+            //var path = @"C:\Users\s19027\Desktop\dane.csv";
+            var path = @"C:\Users\playe\OneDrive\Pulpit\dane.csv";
+            try
+            {
+                var lines = File.ReadLines(path);
+
+
+
+
+
+
+
+
+
+
+
+
+            }catch(Exception e)
+            {
+                if (e is IOException)
+                {
+                    Console.Error.WriteLine("Podana ścieżka jest niepoprawna");
+                }
+                if(e is FileNotFoundException)
+                {
+                    Console.Error.WriteLine("Plik nazwa nie istnieje");
+                }
+            }
+
+            // foreach( var line in lines)
+            //     {
+            //         Console.WriteLine(line);
+            //    }
+            var parsedDate = DateTime.Parse("2020-03-09");
+
+            var today = DateTime.Now;
+            //Console.WriteLine(ParsedDate);
+            //Console.WriteLine(today);
+            //Console.WriteLine(today.ToShortDateString());
+            if (string.IsNullOrEmpty(""))
+            { }
+
+
+
         }
     }
 }
